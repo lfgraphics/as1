43575e781eaac965e34c5dc0a5df5658865825e1
Taha Kazmi <96362790+lfgraphics@users.noreply.github.com>
2022-10-29 00:52:40 +0530

    deployed

---
+++ script.js
@@ -0,0 +1,14 @@
+// var num1 = document.getElementById('num1').value;
+// var num2 = document.getElementById('num2').value;
+function sumnum1() {
+    // var sum = //parseInt(num1) + parseInt(num2);
+    //     document.getElementById('num1').value + document.getElementById('num2').value;
+    // document.getElementById('value1').value = sum;
+// parseInt(document.getElementById("value1").value) =(sum = parseInt(num1) + parseInt(num2))
+// }
+
+document.getElementById("value1").value = sum = parseInt(document.getElementById("num1").value) + parseInt(document.getElementById("num2").value);
+}
+function sumnum2() {
+document.getElementById("value2").value = sum = parseInt(document.getElementById("num3").value) + parseInt(document.getElementById("num4").value);
+}
